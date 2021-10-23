dabfbf196f4e7a7c31f73024f6370b0fe0e9f637
aitoredualdnun <aarque@userzoom.com>
2021-10-23 12:06:13 +0200

    Converted Hello World into Health Check endpoint

--- src/main/java/com/example/fun7_test/domain/HealthResource.java
+++ src/main/java/com/example/fun7_test/domain/HealthResource.java
@@ -4,11 +4,11 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
-@Path("/hello-world")
-public class HelloResource {
+@Path("/health-check")
+public class HealthResource {
     @GET
     @Produces("text/plain")
-    public String hello() {
-        return "Hello, World!";
+    public String health() {
+        return "OK";
     }
 }
