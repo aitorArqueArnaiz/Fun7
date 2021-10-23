0fb949138c6446a1b113e1da6deddfdbd62935e2
aitoredualdnun <aarque@userzoom.com>
2021-10-23 13:13:24 +0200

    Added JSON libraries to project and artifact

--- src/main/java/com/example/fun7_test/domain/UsersResource.java
+++ src/main/java/com/example/fun7_test/domain/UsersResource.java
@@ -1,19 +1,30 @@
 package com.example.fun7_test.domain;
 
+import org.json.simple.JSONObject;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
-@Path("/check-services/{userId}")
+
+@Path("/check-services/{userId}/{timezone}/{cc}")
 public class UsersResource {
     @PathParam("userId")
     private String userId;
+    @PathParam("timezone")
+    private String timezone;
+    @PathParam("cc")
+    private String cc;
 
     @GET
     @Produces("application/json")
-    public String services()
+    public JSONObject services()
     {
-        return "OK";
+        JSONObject jo = new JSONObject();
+        jo.put("name", "jon doe");
+        jo.put("age", "22");
+        jo.put("city", "chicago");
+        return jo;
     }
 }
