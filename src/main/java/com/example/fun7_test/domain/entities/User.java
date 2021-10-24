2ec1aec1a19b5d9fecc694d4c029c87d3a5f4192
aitoredualdnun <aarque@userzoom.com>
2021-10-24 10:24:30 +0200

    Added Get and set for User entity for later use

--- src/main/java/com/example/fun7_test/domain/entities/User.java
+++ src/main/java/com/example/fun7_test/domain/entities/User.java
@@ -5,17 +5,51 @@ public class User
     public User()
     {
         this.UserId = 0;
+        this.Name = "";
         this.multiplayer = false;
         this.support = false;
         this.asd = false;
         this.timezone = "";
         this.cc = "";
+        this.savings = 0.0;
+    }
+
+    public int getId()
+    {
+        return UserId;
+    }
+
+    public void setId(int id)
+    {
+        this.UserId = id;
+    }
+
+    public String getName()
+    {
+        return Name;
+    }
+
+    public void setName(String name)
+    {
+        this.Name = name;
+    }
+
+    public double getSavings()
+    {
+        return savings;
+    }
+
+    public void setSavings(double savings)
+    {
+        this.savings = savings;
     }
 
     public int UserId;
+    public String Name;
     public boolean multiplayer;
     public boolean support;
     public boolean asd;
     public String timezone;
     public String cc;
+    private double savings;
 }
