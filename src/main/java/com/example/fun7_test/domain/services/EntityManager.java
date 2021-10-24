95406db1ab90a98f4c48e6288210efcbd8df0ba5
aitoredualdnun <aarque@userzoom.com>
2021-10-24 11:33:56 +0200

    Working on persistance and entities

--- src/main/java/com/example/fun7_test/domain/services/EntityManager.java
+++ src/main/java/com/example/fun7_test/domain/services/EntityManager.java
@@ -1,5 +1,21 @@
 package com.example.fun7_test.domain.services;
 
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class EntityManager
 {
+    public EntityManager()
+    {
+        this.entityManagerFactory = Persistence.createEntityManagerFactory("User");
+        this.entityManager = (EntityManager) entityManagerFactory.createEntityManager();
+    }
+
+    public EntityManager GetUsersEntityManager()
+    {
+        return this.entityManager;
+    }
+
+    private EntityManagerFactory entityManagerFactory;
+    private EntityManager entityManager;
 }
