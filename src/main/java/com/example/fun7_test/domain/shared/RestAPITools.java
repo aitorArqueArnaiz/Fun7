6a9c45e4c9740eb0a1577064d930246fa02a17d6
aitoredualdnun <aarque@userzoom.com>
2021-10-24 11:43:55 +0200

    Added REST Framework client for using later for user ads

--- src/main/java/com/example/fun7_test/domain/shared/RestAPITools.java
+++ src/main/java/com/example/fun7_test/domain/shared/RestAPITools.java
@@ -1,4 +1,55 @@
 package com.example.fun7_test.domain.shared;
 
-public class RestAPITools {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class RestAPITools
+{
+    public RestAPITools()
+    {
+    }
+
+    public boolean SendRequest()
+    {
+        try
+        {
+
+            URL url = new URL("https://us-central1-o7tools.cloudfunctions.net/fun7-ad-partner");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/json");
+
+            if (conn.getResponseCode() != 200) {
+                throw new RuntimeException("Failed : HTTP error code : "
+                        + conn.getResponseCode());
+            }
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    (conn.getInputStream())));
+
+            String output;
+            System.out.println("Output from Server .... \n");
+            while ((output = br.readLine()) != null) {
+                System.out.println(output);
+            }
+
+            conn.disconnect();
+
+        }
+        catch (MalformedURLException e)
+        {
+            e.printStackTrace();
+            return false;
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
 }
